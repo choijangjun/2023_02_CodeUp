20aee5e10e34fe0c57a5165207d0eac76f69cdf2
choijangjun <jangjun9644@gmail.com>
2023-02-23 18:15:43 +0900

    코드업 1072 문제풀이중

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		System.out.println(x);
+		
+		
+		
+		int i = 0;
+		while (i < x) {
+			System.out.println(a);
+			i++;
+			}
+		
+
+	}
+
+}
