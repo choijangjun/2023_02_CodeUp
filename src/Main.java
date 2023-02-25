14e639c45a7175326ce455ad4b29a1f5236127e7
choijangjun <jangjun9644@gmail.com>
2023-02-25 19:45:19 +0900

    코드업 1084번 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		// 1~9중 정수가 하나 입력되면 일 부터 그 수까지 띄어쓰기
-		// 간격으로 출력하며
-		// 3, 6, 9대신 X가 출력된다.
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] NoT = br.readLine().split(" ");
 
-		// 정수를 하나 입력받는다. o
-		int num = sc.nextInt();
-
-		// 입력받은 정수가 나올 때 까지 일 부터 차례대로 출력한다.o
-		for (int i = 1; i <= num; i++) {
-			if (i == 3 || i == 6 || i == 9) {
-				System.out.printf("X ");
-			} else {
-				System.out.printf(i + " ");
-
+		int sumNoT = 0;
+		for (int i = 0; i < Integer.valueOf(NoT[0]); i++) {
+			for (int x = 0; x < Integer.valueOf(NoT[1]); x++) {
+				for (int y = 0; y < Integer.valueOf(NoT[2]); y++) {
+					bw.write(i + " " + x + " " + y + "\n");
+					sumNoT++;
+				}
 			}
-
 		}
-
+		bw.write(String.valueOf(sumNoT));
+		bw.flush();
 	}
-
 }
