dc0909269b33466b6821156e8879adfe6e988a95
choijangjun <jangjun9644@gmail.com>
2023-02-25 18:06:59 +0900

    코드업 1081번 문제 완료

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,16 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
-		int overSum = sc.nextInt();
-		int sum = 0;
-		int maxNum = 0;
+		int maxNum1 = sc.nextInt();
+		int maxNum2 = sc.nextInt();
 		
-		for (int i = 1; sum <= overSum; i++) {
-			
-			if (sum >= overSum) {
-				break;
+		for(int i = 1; i <= maxNum1; i++) {
+			for(int x = 1; x <= maxNum2; x++) {
+				System.out.println(i+" "+x);
+				
 			}
-			maxNum = i;
-			sum += i;
 		}
-		System.out.println(maxNum);
+
 	}
 
 }
