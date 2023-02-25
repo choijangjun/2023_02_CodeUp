0f615ae78e4cf442b6d0ba0d9100331c8af37377
choijangjun <jangjun9644@gmail.com>
2023-02-25 18:44:33 +0900

    코드업 1082번 문제 풀이 수정

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ public class Main {
 		String dan = sc.nextLine();
 		// dan을 다시 10진수로 바꾼다.o
 		int num = Integer.valueOf(dan, 16);
-		System.out.println(num);
+	
 				
 		for (int i = 1; i <= 15; i++) {
 			// 10진수 구구단 구현
-			System.out.printf("%x * %x = %x\n", num, i, num * i);
+			System.out.printf("%X * %X = %X\n", num, i, num * i);
 			//이걸 16진수 구구단으로 바꾼다.
 		}
 
