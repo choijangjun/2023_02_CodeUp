9ae721644a35d491693808ac0a2906364de15749
choijangjun <jangjun9644@gmail.com>
2023-02-25 14:47:18 +0900

    코드업 1078번 문제 완료

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,24 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		ArrayList<Integer> a = new ArrayList<Integer>();
+		
 		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		System.out.println(x);
 		
+		int num = sc.nextInt();
+		int sum = 0; 
 		
-		
-		int i = 0;
-		while (i < x) {
-			System.out.println(a);
-			i++;
+		for (int i = 1; i <= num; i++) {
+			if(i % 2 == 0) {
+				sum += i;
 			}
+		}
+		System.out.println(sum);
 		
-
+		
+		
+		
+		
 	}
 
 }
