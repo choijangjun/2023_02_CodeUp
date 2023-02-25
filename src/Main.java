be57ce342cec43bb85d4fb5781911b178f4c4299
choijangjun <jangjun9644@gmail.com>
2023-02-25 15:06:24 +0900

    코드업 1079번 문제 완료

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,18 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		int num = sc.nextInt();
-		int sum = 0; 
-		
-		for (int i = 1; i <= num; i++) {
-			if(i % 2 == 0) {
-				sum += i;
+
+		String str = sc.nextLine();
+		String[] arrStr = str.split(" ");
+
+		for (int i = 0; i < arrStr.length; i++) {
+			System.out.println(arrStr[i]);
+			if (arrStr[i].equals("q")) {
+				break;
 			}
 		}
-		System.out.println(sum);
-		
-		
-		
-		
-		
 	}
 
 }
